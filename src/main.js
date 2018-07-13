53fb5ba872bf47c0e719e5cc9968da69841c2161
Steve Winton <stevewinton@gmail.com>
2018-07-13 17:49:35 -0500

    Remove redundancies from main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,3 @@
-function main() {
-    Logger.log(payload.ref)
-    Logger.log(`${strftime('%a, %d %b %Y', new Date())}`)
-    Logger.log(sendEmail(
-        {
-            name: "Sender", 
-            address: "from@sender.org"
-        }, [
-            "to@recipient.org"
-        ], 
-        payload
-    ))
-}
-
-function doGet(e) {
-    var params = JSON.stringify(e);
-    return ContentService.createTextOutput(params);
-  }
-
 function doPost(e) {
     const data = JSON.parse(e.postData.contents)
     const scriptProperties = PropertiesService.getScriptProperties()
